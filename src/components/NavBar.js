ed52c53fafc0d4fa28cf45934db99103be2cc37d
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-10-13 09:36:49 +0300

    Adds the NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,38 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+
+// condtitionally style the active Link
+const isActiveLink = ({ isActive }) => {
+  return {
+     fontWeight : isActive ? "bold" : "normal",
+     color: isActive ? "white" : "black",
+  }
+}
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <nav className="navbuttons">
+      <NavLink exact to="/"  style= {isActiveLink}>
+        Home
+      </NavLink>
+
+      <NavLink to="/about" style={isActiveLink}>
+        Who we Are
+        </NavLink>
+      <NavLink to="/productlist" style={isActiveLink}>
+        Resources
+      </NavLink>
+
+      <NavLink to="/mycart" style={isActiveLink}>
+        What We Do
+      </NavLink>
+      <NavLink to="/reviews" style={isActiveLink}>
+        Council
+      </NavLink>
+
+      <NavLink to="/sign" style={isActiveLink}>
+        Join Us
+      </NavLink>
+
+    </nav>)
 }
