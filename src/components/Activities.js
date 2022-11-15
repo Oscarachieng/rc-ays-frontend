9a0ce9049eb94bc1e0aa12c1b9c8a6f3730f65ce
Oscarachieng <95440374+Oscarachieng@users.noreply.github.com>
2022-11-15 17:08:02 +0000

    Adds pagination to the resources and activities lists

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactPaginate from 'react-paginate';
 import ACtivityCard from "./ACtivityCard";
 
 function Activities({ activities }) {
+
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 4;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(activities.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(activities.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, activities]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % activities.length;
+    setItemOffset(newOffset);
+  };
+   
   return (
     <div className="activities">
       <div className="right">
         <h3>Upcoming Activities/Events</h3>
-        {activities.map((activity) => (
+        {currentItems.map((activity) => (
           <ACtivityCard key={activity.id} activity={activity} />
         ))}
+           <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        containerClassName='pagination'
+        previousLinkClassName="page-num"
+        nextLinkClassName="page-num"
+        activeLinkClassName="active"
+      />
       </div>
     </div>
   );
--- src/components/Resources.js
+++ src/components/Resources.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactPaginate from 'react-paginate';
 import ResourceCard from "./ResourceCard";
 
 
 
 function Resources({resources, setResources}) {
 
-  console.log(resources)
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 4;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(resources.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(resources.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, resources]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % resources.length;
+    setItemOffset(newOffset);
+  };
+   
   
   return (
     <div className="resources">
       <h1>Our Materials</h1>
-      {resources.map((resource) => <ResourceCard key={resource.product_id} resource={resource} setResources={setResources}/>
+      {currentItems.map((resource) => <ResourceCard key={resource.product_id} resource={resource} setResources={setResources}/>
       )}
-    
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        containerClassName='pagination'
+        previousLinkClassName="page-num"
+        nextLinkClassName="page-num"
+        activeLinkClassName="active"
+      />
     </div>
   );
 }
